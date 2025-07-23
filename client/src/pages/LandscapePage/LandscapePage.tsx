1dfc1f0fcb313d68658e1c5cd62152cd90005815
anne durand <annedurand0709@gmail.com>
2025-07-23 10:40:36 +0200

    create page photo

--- client/src/pages/LandscapePage/LandscapePage.tsx
+++ client/src/pages/LandscapePage/LandscapePage.tsx
@@ -1,9 +1,21 @@
+import { useEffect, useState } from "react";
 import "./LandscapePage.css";
 
 function Landscape() {
+  const [photo, setPhoto] = useState<Photo>([]);
+
+  useEffect(() => {
+    fetch(`http://localhost:3310/api/photo`)
+      .then((res) => res.json())
+      .then((data) => setPhoto(data));
+  });
+
   return (
     <main>
       <h1>paysages</h1>
+      <figure>
+        <img src={photo.image} alt={photo.title} />
+      </figure>
     </main>
   );
 }
